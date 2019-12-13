cc5d32403bd1696b409bdd2fd55cc781867a44b6
yakann <mahmut_yakan44@hotmail.com>
2019-12-13 21:26:26 +0300

    ilk commit

---
+++ eticaret_proje/ETicaret/ETicaret/Controllers/BaseController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ETicaret.DB;
+
+namespace ETicaret.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected ETicaretEntities context { get; private set; }
+        public BaseController()
+        {
+            context = new ETicaretEntities();
+            ViewBag.MenuCategories = context.Categories.Where(x => x.Parent_Id == null).ToList();
+        }
+        protected DB.Members CurentUser()
+        {
+            if (Session["LogonUser"] == null) return null;
+            return (DB.Members)Session["LogonUser"];
+        }
+        protected int CurrentUserId()
+        {
+            if (Session["LogonUser"] == null) return 0;
+            return ((DB.Members)Session["LogonUser"]).Id;
+        }
+        protected bool IsLogon()
+        {
+            if (Session["LogonUser"] == null)
+                return false;
+            else
+                return true;
+        }
+    }
+}
